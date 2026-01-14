4188f4da26995884b84c1285e9a462f823ce8eba
Script Raccoon <scriptraccoon@gmail.com>
2026-01-14 21:23:43 +0100

    redirect authenticated users automatically

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,8 +8,13 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const requires_auth =
 		event.url.pathname.startsWith('/account') || event.url.pathname.startsWith('/links')
 
+	const requires_guest =
+		event.url.pathname.startsWith('/login') || event.url.pathname.startsWith('/register')
+
 	if (requires_auth && !event.locals.user) {
 		redirect(307, '/login')
+	} else if (requires_guest && event.locals.user) {
+		redirect(307, '/links')
 	}
 
 	return await resolve(event)
